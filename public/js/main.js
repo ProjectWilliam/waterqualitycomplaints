d5008046c33a4c2b7187557bba8a7742390e98a4
williamguerrero <william.e.guerr@gmail.com>
2018-01-17 19:51:39 -0500

    BETA - Base functionality

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([40.7128, 74.0060], 12);
+var CartoDB_Positron = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
+}).addTo(map);
--- server.go
+++ server.go
@@ -1,1 +1,34 @@
-package waterqualitycomplaints
+package main
+
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	e := echo.New()
+	e.File("/", "public/index.html")
+	e.GET("/data", getJSON)
+	e.Static("/", "public")
+	e.Logger.Fatal(e.Start(":8000"))
+}
+func getJSON(c echo.Context) error {
+	url := "https://data.cityofnewyork.us/resource/qfe3-6dkn.json"
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseString := string(responseData)
+	return c.String(http.StatusOK, responseString)
+
+}
